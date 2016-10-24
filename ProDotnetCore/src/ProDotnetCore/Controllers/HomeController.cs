51fb9b94a407dab12c81d12da2c659a85f9967a8
Mengshi.Lin <linmshi@qq.com>
2016-10-24 22:50:49 +0800

    chapter2

--- ProDotnetCore/src/ProDotnetCore/Controllers/HomeController.cs
+++ ProDotnetCore/src/ProDotnetCore/Controllers/HomeController.cs
@@ -3,33 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProDotnetCore.Models;
 
 namespace ProDotnetCore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ViewResult Index()
+        {
+            ViewBag.Date = DateTime.Now;
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
         {
             return View();
         }
 
-        public IActionResult About()
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
+            Repository.AddResponse(guestResponse);
+            return View("Thanks",guestResponse);
         }
     }
 }

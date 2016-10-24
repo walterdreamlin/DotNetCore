22206a1836021990b17c9e03251afa424f0b078f
Mengshi.Lin <linmshi@qq.com>
2016-10-24 23:44:32 +0800

    listresponses.cshtml

--- ProDotnetCore/src/ProDotnetCore/Controllers/HomeController.cs
+++ ProDotnetCore/src/ProDotnetCore/Controllers/HomeController.cs
@@ -27,5 +27,10 @@ namespace ProDotnetCore.Controllers
             Repository.AddResponse(guestResponse);
             return View("Thanks",guestResponse);
         }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
     }
 }

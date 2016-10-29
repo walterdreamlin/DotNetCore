0b1fcf8c1c2ef64cef34afc41f9115928baa1b92
linmengshi <linmshi@qq.com>
2016-10-29 18:22:01 +0800

    identity

---
+++ SportsStore/src/SportsStore/Models/AppIdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class AppIdentityDbContext:IdetityDbContext<IdentityUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options) { }
+    }
+}
--- SportsStore/src/SportsStore/Startup.cs
+++ SportsStore/src/SportsStore/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Logging;
 using SportsStore.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 
 namespace SportsStore
@@ -31,6 +32,14 @@ namespace SportsStore
             options.UseSqlServer(
                 Configuration["Data:SportStoreProducts:ConnectionString"]));
             //services.AddTransient<IProductRepository, FakeProductRepository>();
+
+            services.AddDbContext<ApplicationDBContext>(options =>
+            options.UseSqlServer(
+                Configuration["Data:SportStoreIdentity:ConnectionString"]));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppIdentityDbContext>();
+
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
@@ -53,6 +62,7 @@ namespace SportsStore
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseSession();
+            app.UseIdentity();
             //app.UseMvcWithDefaultRoute();
             app.UseMvc(routes =>
             {
@@ -85,7 +95,7 @@ namespace SportsStore
                     template: "{controller}/{action}/{id?}");
             });
             SeedData.EnsurePopulated(app);
-
+            IdentitySeedData.EnsurePopulated(app);
         }
     }
 }

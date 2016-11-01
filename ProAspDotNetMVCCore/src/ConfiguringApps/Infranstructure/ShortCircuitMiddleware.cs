d04e3a13802e4e1e1ecb4dc585efe1933b61e9ee
linmengshi <linmshi@qq.com>
2016-11-01 18:37:04 +0800

    shortcircuitmiddleware

---
+++ ProAspDotNetMVCCore/src/ConfiguringApps/Infranstructure/ShortCircuitMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfiguringApps.Infranstructure
+{
+    public class ShortCircuitMiddleware
+    {
+        private RequestDelegate nextDelegate;
+
+        public ShortCircuitMiddleware(RequestDelegate next)
+        {
+            nextDelegate = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if(httpContext.Request.Headers["User-Agent"]
+                .Any(h => h.ToLower().Contains("edge")))
+            {
+                httpContext.Response.StatusCode = 403;
+            }
+            else
+            {
+                await nextDelegate.Invoke(httpContext);
+            }
+        }
+    }
+}
